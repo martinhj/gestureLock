f2ef1a6e2268117675d886243587a75c0d6244d8
Martin Holt Juliussen <martinhj@gmail.com>
2014-10-09 15:52:17 +0200

    Refactoring of the code.

--- gestures.ino
+++ gestures.ino
@@ -1,4 +1,15 @@
-//https://learn.adafruit.com/lsm303-accelerometer-slash-compass-breakout/calibration
+/*
+ *
+ * This sketch makes the use of the compass and accelerometer LSM303DLHC
+ * breakout board from adafruit. It also uses the libraries for this breakout
+ * board.  It recognizes gestures in a simplified maner by checking if certain
+ * thresholds is breached.
+ *
+ * It also uses an adapted smoothing method, smoothing all 6 axis from the
+ * LSM303 sensor. It puts several readings in an array and calculates the
+ * average.
+*/
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
@@ -12,24 +23,38 @@ float xmax = 0;
 float x = 0;
 const float eGravity = 9.78;
 const float compass = 60.0;
+/*
+ * variables for smoothing of the readings.
+ */
 const int numberOfAxis = 6;
-const int numberOfReadings = 18 * 2;
+const int numberOfReadings = 36; // number of readings to run average from.
 int index = 0;
-// readings from the calibration run.
-//acc x, y z, mag x, y, z:
+
+// readings from the calibration run. Using the calibration sketch from Adafruit
+// to find these.
+//https://learn.adafruit.com/lsm303-accelerometer-slash-compass-breakout/calibration
+// the order of the readings stored in the array:
+// acc x, y z, mag x, y, z:
+// calibration run lowest readings.
 float minReadings[numberOfAxis] = 
   {-13.45, -13.18, -10.83, -43.27, -50.0, -56.12};
+// calibration run highest readings.
 float maxReadings[numberOfAxis] = 
   {13.61, 11.96, 11.34, 57.09, 49.73, 52.04};
 float readings[numberOfAxis];
 float readingSums[numberOfAxis];
 float sreadings[numberOfAxis][numberOfReadings];
-unsigned long lastprint = 0;
 
-int ledPinBlue = 11;
-int ledPinGreen = 9;
-int ledPinRed = 10;
-double pi2 = 1570;
+
+struct RGBpins {
+  int blue;
+  int green;
+  int red;
+};
+
+RGBpins ledpins = { 11, 9, 10 };
+
+double ledPulseCounter = 0;
 
 boolean orangeLightOn = false;
 boolean greenLightOn = false;
@@ -47,15 +72,16 @@ boolean unlocked = false;
 
 void setup (void) {
   Serial.begin(115200);
-  if (!mag.begin()) { Serial.println("w: mag"); }
-  if (!accel.begin()) { Serial.println("w: accel"); }
+
   if (!accel.begin() || !mag.begin()) {
-    Serial.println("din't work out, did it?");
+    Serial.println("din't work out. check your LSM303DLHC wiring.");
+    Serial.println("going into a infinite loop now. press reset after rewiring.");
     while(1);
   }
-  pinMode(ledPinBlue, OUTPUT);
-  pinMode(ledPinGreen, OUTPUT);
-  pinMode(ledPinRed, OUTPUT);
+
+  pinMode(ledpins.blue, OUTPUT);
+  pinMode(ledpins.green, OUTPUT);
+  pinMode(ledpins.red, OUTPUT);
 } 
 
 
@@ -76,51 +102,96 @@ void loop(void) {
   populateReadings();
 
 
-
-  //    0  1 2      3  4  5
-  //acc x, y z, mag x, y, z:
-  if (map(2, eGravity) > 9.2f && !unlocked) {
-    orangeLight(true);
-    liftedUpTime = millis();
-    Serial.print(firstLiftedUpTime);
-    if (firstLiftedUpTime == 0) {
-      Serial.println("Lifted up first time reg.");
-      firstLiftedUpTime = liftedUpTime;
-    }
-    liftedUp = true;
+  if (liftUp()) {
+    runLiftUpSequence();
   }
 
-  if (!unlocked && millis() - liftedUpTime > 10000) {
-    liftedUp = false;
+  if (timeOut()) {
+    runTimeOutSequence();
   }
-  if (!unlocked && firstLiftedUpTime != 0 
-    && millis() - firstLiftedUpTime > 9000) {
+
+  if (wrongPassTimeout()) {
+    wrongPassReset();
+  }
+
+  if (correctGesture()) {
+    unlock();
+  }
+
+  //printReadings();
+
+  if (!liftedUp && !unlocked && !wrongPass) {
+    ledPulse();
+  }
+
+  ///*
+  Serial.print(liftedUp); Serial.print(" : ");
+  Serial.print(unlocked); Serial.print(" : ");
+  Serial.print(wrongPass); Serial.println();
+  //*/
+}
+
+
+
+/* checks if the sensor is tilted the right way (earth gravity on the x axis > 6
+ * (check your readings with uncomment of printReadings())) and if it's shaken
+ * at the same time (above or below 3 on the y axis)
+ */
+boolean correctGesture() {
+  return map(0, eGravity) > 6 
+  && (map(1, eGravity) > 3 || map(1, eGravity) < -3);
+}
+
+
+void unlock() {
+    firstLiftedUpTime = 0;
+    greenLight(true);
+    unlocked = true;
+}
+
+boolean timeOut() {
+  int timeoutLength = 10000;
+  return !unlocked && firstLiftedUpTime != 0 
+    && millis() - firstLiftedUpTime > timeoutLength;
+}
+
+void runTimeOutSequence() {
     orangeLight(false);
     redLight(true);
     wrongPass = true;
     wrongpassTime = millis();
     firstLiftedUpTime = 0;
-  }
-  if (firstLiftedUpTime != 0 && millis() - firstLiftedUpTime > 20000) {
-    Serial.println(firstLiftedUpTime);
-    firstLiftedUpTime = 0;
-  }
-  if (!unlocked && wrongPass && millis() - wrongpassTime > 4000) {
-    redLight(false);
-    wrongPass = false;
-  }
-  if (map(0, eGravity) > 6 && (map(1, eGravity) > 3 || map(1, eGravity) < -3)) {
-    greenLight(true);
-    unlocked = true;
-  }
+    liftedUp = false;
+}
 
-  printReadings();
-  if (!liftedUp && !unlocked && !wrongPass) ledPulse();
-  /*
-  Serial.print(liftedUp); Serial.print(" : ");
-  Serial.print(unlocked); Serial.print(" : ");
-  Serial.print(wrongPass); Serial.println();
-  */
+
+boolean wrongPassTimeout() {
+  int timeoutLength = 4000;
+  return !unlocked && wrongPass && millis() - wrongpassTime > timeoutLength;
+}
+void wrongPassReset() {
+  firstLiftedUpTime = 0;
+  redLight(false);
+  wrongPass = false;
+}
+
+
+/* checks if the z-axis (the '2' variable in the readings array) is larger than
+ * earth's gravity (9.2 is not larger than 9.8, but is like that because of
+ * inaccurate calibration - uncomment the printReadings() call above to look at
+ * your readings.
+ */
+boolean liftUp() {
+  return map(2, eGravity) > 9.2f && !unlocked && !wrongPass;
+}
+
+void runLiftUpSequence() {
+  orangeLight(true);
+  liftedUpTime = millis();
+  if (firstLiftedUpTime == 0) {
+    firstLiftedUpTime = liftedUpTime;
+  }
+  liftedUp = true;
 }
 
 
@@ -155,6 +226,9 @@ float populateReadings() {
 
 
 
+/*
+ *
+ */
 float reading(int lindex) {
   return readingSums[lindex]/numberOfReadings;
 }
@@ -163,48 +237,52 @@ float reading(int lindex) {
 
 void orangeLight(boolean onoff) {
   if (onoff) {
-    analogWrite(ledPinGreen,250);
-    analogWrite(ledPinRed, 225);
-    analogWrite(ledPinBlue, 0);
+    analogWrite(ledpins.green,250);
+    analogWrite(ledpins.red, 225);
+    analogWrite(ledpins.blue, 0);
   } else {
-    analogWrite(ledPinGreen, 0);
-    analogWrite(ledPinRed, 0);
+    analogWrite(ledpins.green, 0);
+    analogWrite(ledpins.red, 0);
   }
 }
 
 void redLight(boolean onoff) {
   if (onoff) {
-    analogWrite(ledPinRed, 225);
-    analogWrite(ledPinBlue, 0);
-    analogWrite(ledPinGreen, 0);
+    analogWrite(ledpins.red, 225);
+    analogWrite(ledpins.blue, 0);
+    analogWrite(ledpins.green, 0);
   } else {
-    analogWrite(ledPinRed, 0);
+    analogWrite(ledpins.red, 0);
   }
 }
 
 void greenLight(boolean onoff) {
   if (onoff) {
-    analogWrite(ledPinGreen, 255);
-    analogWrite(ledPinBlue, 0);
-    analogWrite(ledPinRed, 0);
+    analogWrite(ledpins.green, 255);
+    analogWrite(ledpins.blue, 0);
+    analogWrite(ledpins.red, 0);
   } else {
-    analogWrite(ledPinGreen, 0);
+    analogWrite(ledpins.green, 0);
   }
 }
 
+
+
+/*
+ * method that pulses the led when in idle mode.
+ */
 void ledPulse() {
-    analogWrite(ledPinBlue, map((long)(sin(pi2++/200)*100), -100, 100, 0, 255));
-    //Serial.println( map((long)(sin(pi2++/100)*100), -100, 100, 0, 255));
+    analogWrite(ledpins.blue, 
+      map((long)(sin(ledPulseCounter++/200)*100), -100, 100, 0, 255));
 }
 
 
-boolean detectLiftUp() {
-  orangeLightOn = true;
-  return false;
 
-}
-
-
+/*
+ * prints the readings to the serial port. Use a serial monitor to look at
+ * readings. remember to match the baud rate of this sketch in your serial
+ * monitor.
+ */
 void printReadings() {
   Serial.print(map(0, eGravity));
   Serial.print(":");
